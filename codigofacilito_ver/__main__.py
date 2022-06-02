94e06a3cb92747d7fb82b48739d3752149593ad0
EliasGG <elius.gutgall@gmail.com>
2022-06-02 17:48:13 -0400

    Modulo logging

--- codigofacilito_ver/__main__.py
+++ codigofacilito_ver/__main__.py
@@ -1,9 +1,21 @@
+import logging
 from codigofacilito_ver import unreleased
 
+"""
+INFO -> 10
+DEBUG -> 20
+WARNING -> 30
+ERROR -> 40
+CRITICAL -> 50
+"""
+
+#logging.basicConfig(level=logging.DEBUG) # Para forzar a que se muestre por consola los DEBUG
+logging.basicConfig(level=logging.WARNING) # Para forzar a que se muestre por consola los WARNING
+
 if __name__ == '__main__':
-    print('>>>> El paquete ha comnezado a ejecutarse.')
+    logging.debug('>>>> El paquete ha comnezado a ejecutarse.')
 
     workshops = unreleased()
     print(workshops)
 
-    print('>>>> La ejecución de este paquete ha finalizado.')
+    print('>>>> La ejecucion de este paquete ha finalizado.')
